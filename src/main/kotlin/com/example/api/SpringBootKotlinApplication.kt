10110778ffb19797a4fd5ee7bc0d6a69e8644029
Anirban Chatterjee <canirban99@gmail.com>
2020-03-27 00:17:56 +0100

    Code cleanup

--- src/main/kotlin/com/example/api/SpringBootKotlinApplication.kt
+++ src/main/kotlin/com/example/api/SpringBootKotlinApplication.kt
@@ -9,15 +9,3 @@ class SpringBootKotlinApplication
 fun main(args: Array<String>) {
 	runApplication<SpringBootKotlinApplication>(*args)
 }
-
-
-// https://www.javaguides.net/2019/01/springboot-postgresql-jpa-hibernate-crud-restful-api-tutorial.html
-// https://www.callicoder.com/kotlin-spring-boot-mysql-jpa-hibernate-rest-api-tutorial/
-
-
-// complete all the controllers
-// write exception handler
-// write flyway script
-// write service class
-// write unit tests
-// write integration tests
--- src/main/kotlin/com/example/api/model/Employee.kt
+++ src/main/kotlin/com/example/api/model/Employee.kt
@@ -20,20 +20,3 @@ data class Employee (
         @Column(name = "email_address", nullable = true)
         var emailId: String? = null
 )
-
-// if you give default values to all the fields - empty constructor is generated automatically by Kotlin.
-// on the JVM, if all of the parameters of the primary constructor have default values, the compiler will generate an additional parameter less constructor which will use the default values.
-// data classes also derives the implementation of standard methods like equals(), hashCode(), toString(), copy() and componentN() from the properties declared in the data class’s primary constructor.
-// the compiler automatically generates a default getter and setter for all the mutable properties(var), and a getter only for all the read-only properties(val) of the data class.
-// make values nullable by adding ? and then you can assign null.
-// The language's type system differentiates between nullable and non-nullable types and every class can be used in both versions. By default, a reference of type String cannot hold null, whereas String? allows it.
-// declare a variable with nullable String type, it's OK to assign `null` to it    https://kotlinexpertise.com/kotlin-nullability-features/
-// var b: String? = "possiblyNull"
-// https://www.baeldung.com/kotlin-jpa
-
-
-//Every Data Class in Kotlin needs to fulfill the following requirements -  https://kotlinexpertise.com/kotlinquickreference/
-//
-//The primary constructor must have at least one parameter
-//All the parameters declared in the primary constructor need to be marked as val or var.
-//Data classes cannot be abstract, open, sealed or inner.
