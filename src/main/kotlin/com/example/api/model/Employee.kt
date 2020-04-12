aa2b1041be941d3afaeb7a8179e7bdec86af6d19
Anirban Chatterjee <canirban99@gmail.com>
2020-04-12 12:56:55 +0200

    Fixed entity fields with non-nullable types

--- src/main/kotlin/com/example/api/model/Employee.kt
+++ src/main/kotlin/com/example/api/model/Employee.kt
@@ -13,10 +13,12 @@ import javax.persistence.Table
 data class Employee (
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Long = 0,
-        @Column(name = "first_name", nullable = true)
-        var firstName: String? = null,
-        @get:Column(name = "last_name", nullable = true)  //@get annotate Java getter
-        var lastName: String? = null,
-        @Column(name = "email_address", nullable = true)
-        var emailId: String? = null
+        @Column(name = "first_name", nullable = false)
+        var firstName: String = "",
+        @Column(name = "middle_name", nullable = true)
+        var middleName: String? = "",
+        @Column(name = "last_name", nullable = false)
+        var lastName: String = "",
+        @Column(name = "email_address", nullable = false)
+        var emailId: String = ""
 )
